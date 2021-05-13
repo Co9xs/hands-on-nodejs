cb0a0118582d49f8b0e7e8ff5fb559efca76b065
Co9xs <ryo.fjsm0203@icloud.com>
2021-05-13 11:26:27 +0900

    P50:コールバックとエラーハンドリング

---
+++ src/errro-hadnling.js
@@ -0,0 +1,31 @@
+// try-catchでエラーをキャッチできる
+function parseJSONSync(json) {
+  try {
+    return JSON.parse(json)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// callback内のエラーはキャッチできない
+function parseJSONSyncCallback(json, callback) {
+  try {
+    setTimeout(() => {
+      callback(JSON.parse(json))
+    }, 1000)
+  } catch (error) {
+    console.error(error)
+    callback({})
+  }
+}
+
+// nodejsの規約に沿ったパターン(callbackの引数にerrorを渡す)
+function parseJSONSync2 (json, callback) {
+  setTimeout(() => {
+    try {
+      callback(null, JSON.parse(json))
+    } catch (error) {
+      callback(error)
+    }
+  }, 1000)
+}
