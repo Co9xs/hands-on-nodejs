9ffc9bac716015ffb7b56f55b1c42c8ea270422f
Co9xs <ryo.fjsm0203@icloud.com>
2021-05-15 15:36:30 +0900

    P149:sigle-thread

---
+++ src/fibonacci.js
@@ -0,0 +1,3 @@
+module.exports = function fibonacci(n) {
+  return n <= 1 ? n : fibonacci(n-1) + fibonacci(n-2)
+}
---
+++ src/web-app.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const fibonacci = require('./fibonacci')
+
+http.createServer((req, res) => {
+  const n = Number(req.url.substr(1))
+  if (Number.isNaN(n)) {
+    return res.end()
+  }
+
+  const result = fibonacci(n)
+  res.end(result.toString())
+}).listen(3000)
