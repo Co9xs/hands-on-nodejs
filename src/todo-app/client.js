51ba02087f272667d6824093ae97c09b56b3a71a
Co9xs <ryo.fjsm0203@icloud.com>
2021-06-19 16:14:26 +0900

    reset file

--- src/todo-app/client.js
+++
@@ -1,8 +0,0 @@
-const http = require("http")
-
-http.request("http://localhost:3000/api/todos", res => {
-  let responseData = ""
-  console.log(responseData, res.statusCode)
-  res.on("data", chunk => responseData += chunk)
-  res.on("end", () => console.log("responseData", JSON.parse(responseData)))
-}).end()
