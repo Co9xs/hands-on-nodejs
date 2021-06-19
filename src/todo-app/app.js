c61a84ef1a0bdd54d6d261ef17e24dd706e3b4d1
Co9xs <ryo.fjsm0203@icloud.com>
2021-06-19 16:49:10 +0900

    add react and next.js

--- src/todo-app/app.js
+++ src/todo-app/app.js
@@ -1,9 +1,36 @@
 const express = require("express")
+const { nextTick } = require("process")
 let todos = [
   {id: 1, title: "ネーム", completed: false},
   {id: 2, title: "下書き", completed: true},
 ]
 const app = express()
+app.use(express.json())
 
-app.get("/api/todos", (req, res) => res.json(todos))
+app.get("/api/todos", (req, res) => {
+  if (!req.query.completed) {
+    return res.json(todos)
+  }
+  const completed = req.query.completed === 'true'
+  res.json(todos.filter(todo => todo.completed === completed))
+})
+
+let id = 2
+app.post("/api/todos", (req, res, next) => {
+  const { title } = req.body
+  if (typeof title !== "string" || !title) {
+    const err = new Error("title is required")
+    err.statusCode = 400
+    return next(err)
+  }
+  const todo = {id: id += 1, title, completed: false}
+  todos.push(todo)
+  res.status(201).json(todo)
+})
+
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.status(err.statusCode || 500).json({ error: err.message })
+})
+
 app.listen(3000)
