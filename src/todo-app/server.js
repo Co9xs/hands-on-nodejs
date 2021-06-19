be82716cb4474885cda07aa14b103aeee6c98da6
Co9xs <ryo.fjsm0203@icloud.com>
2021-06-19 16:06:25 +0900

    add express

--- src/todo-app/server.js
+++ src/todo-app/server.js
@@ -1,18 +1,13 @@
 const http = require("http")
+const app = require("express")
+
 const todos = [
   {id: 1, title: "ネーム", completed: false},
   {id: 1, title: "ネーム", completed: true},
 ]
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/api/todos") {
-    if (req.method === "GET") {
-      res.setHeader("Content-Type", "application/json")
-      return res.end(JSON.stringify(todos))
-    }
-    res.statusCode = 405
-  } else {
-    res.StatusCode = 404
-  }
-  res.end()
-}).listen(3000)
+app.get("/api/todos", (req, res) => {
+  res.json(todos)
+})
+
+app.listen(3000)
