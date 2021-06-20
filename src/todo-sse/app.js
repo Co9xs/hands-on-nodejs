7e0aca8c0b192156723e0e01d4757cc37f91bb6d
Co9xs <ryo.fjsm0203@icloud.com>
2021-06-20 12:06:30 +0900

    implement simple sse handler

---
+++ src/todo-sse/app.js
@@ -0,0 +1,64 @@
+const express = require("express")
+const next = require("next")
+const dev = process.env.NODE_ENV !== 'production'
+const nextApp = next({ dev })
+
+nextApp.prepare().then(
+  () => app.get('*', nextApp.getRequestHandler()),
+  err => {
+    console.error(err)
+    process.exit(1)
+  }
+)
+
+let todos = [
+  {id: 1, title: "ネーム", completed: false},
+  {id: 2, title: "下書き", completed: true},
+]
+const app = express()
+app.use(express.json())
+
+app.get("/api/todos", (req, res) => {
+  if (!req.query.completed) {
+    return res.json(todos)
+  }
+  const completed = req.query.completed === 'true'
+  res.json(todos.filter(todo => todo.completed === completed))
+})
+
+let sseSenders = []
+let sseId = 1
+
+app.get("/api/todos/events", (req, res) => {
+  req.socket.setTimeout(0)
+  res.set({
+    "Content-Type": "text/event-stream"
+  })
+  const send = (id, data) => res.write(`id: ${id}\ndata: ${data}\n\n`)
+  sseSenders.push(send)
+  send(sseId, JSON.stringify(todos))
+  req.on('close', () => {
+    res.end()
+    sseSenders = sseSenders.filter(_send => _send !== send)
+  })
+})
+
+let id = 2
+app.post("/api/todos", (req, res, next) => {
+  const { title } = req.body
+  if (typeof title !== "string" || !title) {
+    const err = new Error("title is required")
+    err.statusCode = 400
+    return next(err)
+  }
+  const todo = {id: id += 1, title, completed: false}
+  todos.push(todo)
+  res.status(201).json(todo)
+})
+
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.status(err.statusCode || 500).json({ error: err.message })
+})
+
+app.listen(3000)
