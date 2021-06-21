757ccc77bd559dfcefa13248a88c7ff9261c1723
Co9xs <ryo.fjsm0203@icloud.com>
2021-06-21 14:45:00 +0900

    add test case for fetchByCompleted

--- src/todo-data-storage/test/mocha/data-storage.js
+++ src/todo-data-storage/test/mocha/data-storage.js
@@ -7,6 +7,8 @@ for (const dataStorageName of ["file-system", "sqlite"]) {
       const allTodos = await fetchAll()
       await Promise.all(allTodos.map(({id}) => remove(id)))
     })
+
+    // create, fetchAll
     describe("create(), fetchAll()", () => {
       it("create()で作成したTODOをfetchAllで取得できる", async () => {
         assert.deepEqual(await fetchAll(), [])
@@ -22,5 +24,24 @@ for (const dataStorageName of ["file-system", "sqlite"]) {
         assert.sameDeepMembers(await fetchAll(), [todo1, todo2, todo3])
       })
     })
+
+    // fetchByCompleted
+    describe("fetchByCompleted", () => {
+      it("completedの値が引数で指定したものと等しいTodoだけを取得できる", async () => {
+        assert.deepEqual(await fetchAll(), [])
+        assert.deepEqual(await fetchByCompleted(true), [])
+        assert.deepEqual(await fetchByCompleted(false), [])
+
+        const todo1 = { id: "a", title: "ネーム", completed: false }
+        await create(todo1)
+        const todo2 = { id: "b", title: "下書き", completed: true }
+        await create(todo2)
+        const todo3 = { id: "c", title: "ペン入れ", completed: false }
+        await create(todo3)
+        assert.deepEqual(await fetchByCompleted(true), [todo2])
+        assert.sameDeepMembers(await fetchByCompleted(false), [todo1, todo3])
+      })
+    })
+
   })
 }
